9998c4586ec5bd578960899644e0bcacf4e7b4d3
DanishAjma1 <danishajmalbutt@gmail.com>
2024-08-22 17:56:59 +0500

    Devised files

--- BookCRUD/app/controllers/books_controller.rb
+++ BookCRUD/app/controllers/books_controller.rb
@@ -1,44 +1,62 @@
 class BooksController < ApplicationController
-  def index
-    @book = Book.all
-  end
-  def new
-    @book = Book.new
+  # Authenticate user before certain actions
+  
+  protect_from_forgery with: :exception, prepend: true
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  allow_browser versions: :modern
+  def after_sign_in_path_for(resource)
+    root_path # Redirect to the dashboard after sign-in
   end
 
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to books_path, notice: 'Book was successfully created.'
-    else
-      render :new,status: :unprocessable_entity
-    end
+  def after_sign_out_path_for(resource_or_scope)
+    root_path # Redirect to the homepage after sign-out
+  end
+  def index
+    @books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    # @book.user = current_user  # Associate the book with the signed-in user
+
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def edit
     @book = Book.find(params[:id])
-    end
+  end
 
   def update
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to books_path, notice: 'Book was successfully updated.'
+      redirect_to @book, notice: 'Book was successfully updated.'
     else
-      render :edit,status: :unprocessable_entity
+      render :edit
     end
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path, notice: 'Book was successfully deleted.'
-    end
+    redirect_to books_url, notice: 'Book was successfully destroyed.'
+  end
+
   private
+
   def book_params
-    params.require(:book).permit(:title, :author_name,:description)
-    end
+    params.require(:book).permit(:title, :author_name, :description)
+  end
 end
